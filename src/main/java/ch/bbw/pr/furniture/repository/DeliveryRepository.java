d99f32ecf4733c4f3e1bceed8c4618fb976a6e10
Marijaradovanoviic <Marija.Radovanovic@swisscom.com>
2023-01-25 17:43:14 +0100

    Auswertung der Daten mittels SQL Queries im Repo Delivery

--- src/main/java/ch/bbw/pr/furniture/repository/DeliveryRepository.java
+++ src/main/java/ch/bbw/pr/furniture/repository/DeliveryRepository.java
@@ -3,6 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ch.bbw.pr.furniture.model.Delivery;
 
+//Auswertung der Daten mittels SQL Queries 
 public interface DeliveryRepository extends JpaRepository <Delivery, Long>{
  
 }
